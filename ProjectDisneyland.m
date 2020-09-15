14035c8904082fcc6f1b98299f123dd14d81870a
glazar01 <71275439+glazar01@users.noreply.github.com>
2020-09-16 01:26:59 +0300

    Update ProjectDisneyland.m

--- ProjectDisneyland.m
+++ ProjectDisneyland.m
@@ -1,28 +1,30 @@
 % Author: Georgia Lazaridou
 % Last Date Modified: 16/09/2020
-% Description: 
-
+% Description: This m-file consists of a code that solves an orienteering optimization problem in Matlab based on Disneyland's Paris data. There are some attractions with different
+% attributes(=Data) as defined below and there is also a time limit(=Tmax). This program defines the constraints as well as the objective function of this problem and solves it 
+% using Yalmip (with some extra settings) for a specific age group(=Age) which can be 1,2,3 or 4. Each attraction has a different rate for each age group(table Ratings) where
+% column 1 = Preschoolers etc. and the selection of column can be made by choosing the right number for RateGroup variable. Finally, the solution of the problem is printed on 
+% the console.
 
 
 % Define Variables
 N = 46; %number of attractions
 x = binvar (N+2,N+2, 'full'); % attractions binary variables
 u = intvar (N+1,1);  % subtours integer variables
-%tt = t(1:N+1,2:N+2);
 
 % Define Some Values for the constraints
-%Tmax = 60; % number of available minutes
+Tmax = 60; % Number of available time in minutes
 Age = 3; % Preschoolers = 1, Kid = 2, Tween = 3, Adults/Teens = 4
 RateGroup = 3; % Preschoolers = 1, Kid = 2, Tween = 3, Teens = 4, Adults = 5, Kid+Teen+Adult = 6, Preschooler+Tween+Adult = 7
-%RateGroup = 1; 
 
-% Define Data Used
-walk = csvread('MinutesWalkingModified.csv'); % minutes of walking between attractions tij
-Dur = csvread('DurationModified.csv');
-Wait = csvread('WaitTimeModified.csv');
-Ratings = csvread('RatingsModified.csv'); % Preschoolers | Kids | Tweens | Teens | Adults | Kid+Teen+Adult | Preschooler+Tween+Adult
-AgeR = csvread('Age RestrictionsModified.csv'); % Preschoolers, Kids, Tweens, Adults/Teens
+% Define Data Used for each attraction
+walk = csvread('MinutesWalkingModified.csv'); % Minutes of walking between attractions
+Dur = csvread('DurationModified.csv'); % Duration of each attraction
+Wait = csvread('WaitTimeModified.csv'); % Wait time of each attraction
+Ratings = csvread('RatingsModified.csv'); % Columns: Preschoolers | Kids | Tweens | Teens | Adults | Kid+Teen+Adult | Preschooler+Tween+Adult
+AgeR = csvread('Age RestrictionsModified.csv'); % Columns: Preschoolers, Kids, Tweens, Adults/Teens
 
+% Calculation of total spending time for each attraction
 t = zeros(N+2);
 for i = 1 : N+2
     for j = 1 : N+2
@@ -30,11 +32,13 @@ for i = 1 : N+2
     end
 end
 
+% Choosing the right column of 'Ratings' according to the RateGroup choosen
 Rate = Ratings(:,RateGroup);
-AgeRes = AgeR(Age,1:N); % vector consists of restristrictions for age group 1,2,3 or 4
+% Vector consists of restrictions for age group 1,2,3 or 4 (=Column of AgeR)
+AgeRes = AgeR(Age,1:N); 
 
 
-% Define constraints T
+% Define all the constraints of the problem
 Constraints = [sum(sum(x.*t)) <= Tmax, sum(x(1,2:N+2)) == 1, sum(x(1:N+1,N+2)) == 1, sum(x(:,1)) == 0, sum(x(N+2,:)) == 0];
 for i = 1 : N+1
     Constraints = [Constraints, 2 <= u(i) <= N+2];
@@ -51,25 +55,20 @@ for i = 2 : N+2
     end
 end
 for i = 1 : N
-    % if statement to take the restrictions for the specific age group
-    % between the N attractions of table AgeRes
-    if (AgeRes(i) == 1)
+    % if statement to take only the restrictions for the specific age group between the N attractions of table AgeRes
+    if (AgeRes(i) == 1) % If the attraction is not permitted (=1)
         for j = 1 : N+1
             Constraints = [Constraints, x(j,i+1) == 0, x(i+1,j+1) == 0];
         end
     end
 end
 
-% Define an objective
-%r = Rate(:,RateGroup);
-xx = x(1:N+1,2:N+1); %for multiply with rate in objective
-Objective = -sum(sum(xx*Rate));   % 
+% Define the objective of the problem
+xx = x(1:N+1,2:N+1); % xx for multiply with rate in objective because x includes entrance (=first row)
+Objective = -sum(sum(xx*Rate));  
 
 % Set some options for YALMIP and solver
-% options = sdpsettings('verbose',1,'solver','quadprog','quadprog.maxiter',100);
 options = sdpsettings('verbose',1,'solver','gurobi','gurobi.timelimit',7200);
-
-saveampl(Constraints,Objective,'mymodel46_1');
 
 % Solve the problem
 sol = optimize(Constraints,Objective,options);
@@ -81,8 +80,8 @@ if sol.problem == 0
  disp(Tmax);
  solution = value(x)
  solution_u = value(u)
- Attractions = value(sum(sum(x))-1)
- Time = value(sum(sum(x.*t)))
+ Attractions = value(sum(sum(x))-1) % Calculate the number of visited attractions
+ Time = value(sum(sum(x.*t))) % Calculate the total number of minutes wasted
  
 else
  display('Hmm, something went wrong!');
@@ -92,7 +91,7 @@ else
  disp(Tmax);
  solution = value(x)
  solution_u = value(u)
- Attractions = value(sum(sum(x))-1)
- Time = value(sum(sum(x.*t)))
+ Attractions = value(sum(sum(x))-1) % Calculate the number of visited attractions
+ Time = value(sum(sum(x.*t))) % Calculate the total number of minutes wasted
 end
 end
